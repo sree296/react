f47614ad260ca30b8d02502c959d6f0f8a1699a9
sree296 <nivas296@gmail.com>
2018-08-01 09:44:58 +0530

    Basic Header

--- myapp/src/Header/Header.js
+++ myapp/src/Header/Header.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../css/Header/Header.css';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
 
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <h1>Sreenivas</h1>
-      </div>
+      <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
     );
   }
 }
