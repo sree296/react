faf841c7dc2d106ad3a4fb0ff521484a628724dc
sree296 <nivas296@gmail.com>
2021-08-01 00:11:01 +0530

    Input Story

---
+++ story-book/src/components/Input/Input.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Input from './Input';
+
+export default{
+    title: 'Input',
+    component : Input
+}
+
+export const Small = () => <Input size="small"></Input>
+export const Medium = () => <Input size="medium"></Input>
+export const Large = () => <Input size="large"></Input>
