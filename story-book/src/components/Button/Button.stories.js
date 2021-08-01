f796719e5806cf7433af27d46b653a756b083e50
sree296 <nivas296@gmail.com>
2021-08-01 19:59:13 +0530

    Using Decorator for button center

--- story-book/src/components/Button/Button.stories.js
+++ story-book/src/components/Button/Button.stories.js
@@ -4,9 +4,10 @@ import Center from '../Center/Center'
 
 export default {
     title: 'Form/Button',
-    component : Button
+    component : Button,
+    decorators: [story => <Center>{story()}</Center>]
 }
-export const Primary = () => <Center><Button variant="primary">Primary</Button></Center>
+export const Primary = () => <Button variant="primary">Primary</Button>
 export const Secondary = () => <Button variant="secondary">Secondary</Button>
 export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
