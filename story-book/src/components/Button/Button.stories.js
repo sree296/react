08de64b813a52de04636134500f6d3e3d8a4f808
sree296 <nivas296@gmail.com>
2021-08-01 19:57:00 +0530

    Center Button

--- story-book/src/components/Button/Button.stories.js
+++ story-book/src/components/Button/Button.stories.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from './Button';
+import Center from '../Center/Center'
 
 export default {
     title: 'Form/Button',
     component : Button
 }
-export const Primary = () => <Button variant="primary">Primary</Button>
+export const Primary = () => <Center><Button variant="primary">Primary</Button></Center>
 export const Secondary = () => <Button variant="secondary">Secondary</Button>
 export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
